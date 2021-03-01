7707e9e1096b9b89110037f9fef00dc95955bcb1
Божко Александр Викторович <qa_aleksvik@outlook.com>
2021-03-01 20:03:51 +0400

    изменилтметод add()

---
+++ src/com/company/Directory.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.Set;
+
+public interface Directory {
+    void add(String[] surname, String[] phone);
+    Set <String> get(String surname);
+}
--- src/com/company/Phonebook.java
+++ src/com/company/Phonebook.java
@@ -2,53 +2,81 @@ package com.company;
 
 import java.util.*;
 
-public class Phonebook {
-    List<String> contacts = new ArrayList<>();
-    List<String> number = new ArrayList <>();
-    Map<List<String>, List<String>> phoneBook;
-
-    public void add(String[] contact){
-        String[] surname = new String[8];
-        for (String lastName: surname) {
-            List<String> contacts = new ArrayList<>();
-            contacts.add(lastName);
-        }
-
-    }
-    public void get(){
+public class Phonebook implements Directory {
+    private Map<String, Set<String>> phonesBySurname;
+    @Override
+    public void add(String[] surname, String[] phone) {
         Map <String, String> phoneBook = new HashMap <>();
-        for (String surname: phoneBook.values()) {
-            for (String phones: phoneBook.keySet()) {
-                Set<String> phone = phoneBook.keySet();
-                System.out.println(phoneBook.keySet() + ": " + phone);
+        for (int i = 0; i < phone.length ; i++) {
+            for (int j = 0; j < surname.length; j++) {
+                if (i == j) {
+                    phoneBook.putIfAbsent(phone[i], surname[j]);
+                }
             }
-        }
-        
-    }
 
-
-
-    public static void main(String[] args) {
-        List<String> contacts = new ArrayList<>();
-        List<String> number = new ArrayList <>();
-        Map <String, String> phoneBook = new HashMap <>();
-        String[] surname = {"Petrov", "Ivanov", "Popov", "Petrov", "Semenov", "Shestakov", "Leonov", "Semenov"};
-        String[] phone = {"456 789", "270 270", "154 123", "788 695", "785 695", "327 456", "270 270", "147 233"};
-
-        Phonebook phonebook = new Phonebook();
-        phonebook.add(surname);
-        phonebook.add(phone);
-        for (int i = 0; i < surname.length; i++) {
-            for (int j = 0; j < phone.length; j++) {
-                if (i == j)
-                phoneBook.putIfAbsent(phone[i], surname[j]);
-            }
         }
         for (Map.Entry <String, String> o: phoneBook.entrySet()) {
             System.out.println(o.getKey() + ": " + o.getValue());
         }
-        phonebook.get();
+        }
+
+
+
+    @Override
+    public Set <String> get(String surname) {
+        phonesBySurname =
+        return phonesBySurname.putIfAbsent(surname, phonesBySurname.keySet());
+    }
+//    List<String> contacts = new ArrayList<>();
+//    List<String> number = new ArrayList <>();
+//    Map<List<String>, List<String>> phoneBook;
+//
+//    public void add(String[] contact, int arraySize){
+//        String[] surname = new String[arraySize];
+//        for (String lastName: surname) {
+//            List<String> contacts = new ArrayList<>();
+//            contacts.add(lastName);
+//        }
+//
+//    }
+//    public void get(){
+//        Map <String, String> phoneBook = new HashMap <>();
+//        for (String surname: phoneBook.values()) {
+//            for (String phones: phoneBook.keySet()) {
+//                Set<String> phone = phoneBook.keySet();
+//                System.out.println(phoneBook.keySet() + ": " + phone);
+//            }
+//        }
+//    }
+
+    public static void main(String[] args) {
+        Map <String, String> phoneBook = new HashMap <>();
+        String[] surname = {"Petrov", "Ivanov", "Popov", "Petrov", "Semenov", "Shestakov", "Leonov", "Semenov"};
+        String[] phone = {"456 789", "270 270", "154 123", "788 695", "785 695", "327 456", "270 275", "147 233"};
+        Phonebook phonebook = new Phonebook();
+        phonebook.add(surname, phone);
+        phonebook.get("Petrov");
+
+//        Phonebook phonebook = new Phonebook();
+//        phonebook.add(surname, surname.length);
+//        phonebook.add(phone, phone.length);
+//        for (int i = 0; i < surname.length; i++) {
+//            for (int j = 0; j < phone.length; j++) {
+//                if (i == j)
+//                phoneBook.putIfAbsent(phone[i], surname[j]);
+//            }
+//        }
+//        for (Map.Entry <String, String> o: phoneBook.entrySet()) {
+//            System.out.println(o.getKey() + ": " + o.getValue());
+//        }
+//        phonebook.get();
+//
+//    }
+
 
     }
+
+
+
 
 }
