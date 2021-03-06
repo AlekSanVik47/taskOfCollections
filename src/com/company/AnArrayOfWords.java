4c36dbbdb798b803a150b94f7c85b16997b9169d
Божко Александр Викторович <qa_aleksvik@outlook.com>
2021-03-06 17:36:20 +0400

    удаляет из массива дубликаты и оставляет только уникальные слова

--- src/com/company/AnArrayOfWords.java
+++ src/com/company/AnArrayOfWords.java
@@ -1,28 +1,53 @@
 package com.company;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
-public class AnArrayOfWords {
-    private void UniqueArray (String[] words){
-        Set <String> uniqueWords = new HashSet <>();
-        for (int i = 0; i < words.length; i++) {
-            words[i] = words[i].toLowerCase(Locale.ROOT);
-        }
-        Collections.addAll(uniqueWords, words);
-        System.out.println(uniqueWords);
-        System.out.printf("Уникальных слов встречается %s", uniqueWords.size());
-    }
+public class AnArrayOfWords  {
+    Set<String> words = new HashSet <>();
+
+
+ private void UniqueArray (List<String> words){
+     Set <String> uniqueWords = new HashSet <>();
+     String wordCount = "Unique words";
+     for (int i = 0; i < words.size(); i++) {
+         for (String word : words) {
+             if (word.equalsIgnoreCase((words.get(i))))
+                 uniqueWords.remove(word);
+                 uniqueWords.add(words.get(i));
+         }
+     }
+     System.out.printf("%s: %d%n", wordCount, uniqueWords.size());
+     System.out.println(uniqueWords);
+ }
 
     public static void main(String[] args) {
-        String[] words = {"Друзья", "Взгляд", "Глаза",
-                "Игра", "Улыбка", "Радость", "Ученье", "Жизнь", "Игра", "Улыбка",
+        String[] words = {"друзья", "Взгляд", "Глаза",
+                "Игра", "улыбка", "Радость", "Ученье", "Жизнь", "Игра", "Улыбка",
                 "Радость", "Ученье", "Смех", "Успех", "Школа", "Доллар", "Золото",
                 "Погода", "доллар"};
+        List <String> wordsList = new ArrayList <>(Arrays.asList(words));
 
-        AnArrayOfWords anArrayOfWords = new AnArrayOfWords();
-        anArrayOfWords.UniqueArray(words);
+        AnArrayOfWords arrayOfWords = new AnArrayOfWords();
+        arrayOfWords.UniqueArray(wordsList);
+        }
+
+        @Override
+    public String toString() {
+        return new StringJoiner(", ", AnArrayOfWords.class.getSimpleName() + "[", "]")
+                .add("words=" + words)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o ) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnArrayOfWords that = ( AnArrayOfWords ) o;
+        return Objects.equals(words, that.words);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(words);
     }
 }
